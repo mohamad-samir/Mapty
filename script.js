aad5f9fb0b971b2c6f51602d4eabdbc2d148d297
mohamad-samir <mohamad.samir2011@gmail.com>
2024-03-15 23:55:46 +0200

    child classes

--- script.js
+++ script.js
@@ -13,6 +13,41 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 //let map and mapEvent be a globbal variable
 //let map, mapEvent;
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+    this.calcPace();
+  }
+}
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+  }
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+////////////////////////////////////////////////////////
+//                 App Architecture                   //
+////////////////////////////////////////////////////////
 
 class App {
   #map;
